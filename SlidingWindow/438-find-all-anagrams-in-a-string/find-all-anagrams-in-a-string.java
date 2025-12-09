9352f23dd7b2c13a6beeab5a84d4406ac04c7bcd
Divya <137617870+Divyark3@users.noreply.github.com>
2025-12-09 23:05:43 +0530

    Time: 65 ms (23.22%) | Memory: 48.2 MB (5.31%) - LeetSync

---
+++ SlidingWindow/438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+
+        int n = p.length();
+        int m = s.length();
+
+        int start=0, end=0;
+
+        Map<Character, Integer> m1 = new HashMap<>();
+        Map<Character, Integer> m2 = new HashMap<>();
+
+        List<Integer> res = new ArrayList<>();
+
+        for(end=0; end<n; end++){
+            char c = p.charAt(end);
+            m1.put(c, m1.getOrDefault(c,0) + 1);
+        }
+
+        for(end=0; end<m; end++){
+            char c = s.charAt(end);
+            m2.put(c, m2.getOrDefault(c, 0) + 1);
+
+            while(end-start+1 > n){
+
+                char ch = s.charAt(start);
+                m2.put(ch, m2.get(ch) - 1);
+
+                if(m2.get(ch) == 0){
+                    m2.remove(ch);
+                }
+                start++;
+            }
+
+            if(end-start+1 == n && m2.equals(m1)){
+                res.add(start);
+            }
+        }
+        return res;
+        
+    }
+}

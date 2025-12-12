12674f95390bfd620b02d0f249dce41876501849
Divya <137617870+Divyark3@users.noreply.github.com>
2025-12-12 22:58:11 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (35.54%) - LeetSync

---
+++ TwoPointers/75-sort-colors/sort-colors.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void sortColors(int[] nums) {
+
+        int n = nums.length;
+        int r = 0;
+        int w = 0;
+        int b = n-1;
+
+        for(int i=0; i<n; i++){
+
+            if(nums[w] == 0){
+                int temp = nums[w];
+                nums[w] = nums[r];
+                nums[r] = temp;
+                r++;
+                w++;
+            }
+            else if(nums[w] == 2){
+                int temp = nums[w];
+                nums[w] = nums[b];
+                nums[b] = temp;
+                b--;
+            }
+            else{
+                w++;
+            }
+        }
+    }
+}
